0cc2fbc3f6babb07d0dead973dedb15440cb28a8
AbhishekKameri <122601478+AbhishekKameri@users.noreply.github.com>
2023-01-25 14:42:25 +0530

    Add files via upload

--- ArrayL.java
+++ ArrayL.java
@@ -1,59 +1,58 @@
-import java.util.Random;
-import java.util.Scanner;
-class SquareThread implements Runnable {
-int x;
-SquareThread(int x) {
-this.x = x;
-}
-public void run() {
-System.out.println("Thread Name: Square Thread and Square of"+ x +" is: "+ x * x);
-}
-}
-class CubeThread implements Runnable {
-int x;
-CubeThread(int x) {
-this.x = x;
-}
-public void run() {
-System.out.println("Thread Name: Cube Thread and Cube of "+x+" is: "+ x * x * x);
-}
-}
-class RandomThread implements Runnable {
-Random r;
-Thread t2, t3;
-public void run() {
-int num;
-r = new Random();
-try
- {
- Scanner input = new Scanner(System.in);
- System.out.print("Enter the Number of Random Numbers:");
- int n = input.nextInt();
- System.out.println();
-for(int i=0 ; i<n ; i++)
- {
-num = r.nextInt(100);
-System.out.println("Main Thread and Generated Number is: "+ num);
-t2 = new Thread(new SquareThread(num));
-t2.start();
-t3 = new Thread(new CubeThread(num));
-t3.start();
-Thread.sleep(1000);
-System.out.println("--------------------------------------");
-}
-} 
- catch (Exception ex)
- {
-System.out.println("Interrupted Exception");
-}
-}
-}
-public class MainThread
-{
-public static void main(String[] args)
- {
-RandomThread thread_obj = new RandomThread();
-Thread t1 = new Thread(thread_obj);
-t1.start();
-}
-}
+import java.util.*;
+public class ArrayL {
+	ArrayList<String> list =new ArrayList<String>();
+	Scanner in=new Scanner(System.in);
+	public void array_display(){
+		list.add("CSE");
+		list.add("ECE");
+		list.add("MECH");
+		System.out.println("Array list elements are ");
+		System.out.println(list);
+		System.out.println("");
+	}
+	public void append_at_end(){
+		System.out.println("Enter the element to append at end : ");
+		String ele=in.next();
+		list.add(ele);
+		System.out.println(list);
+		System.out.println("");
+	}
+	public void insert_at_pos(){
+		System.out.println("Enter the position and element to be inserted : ");
+		int posindex=in.nextInt();
+		String ele=in.next();
+		list.add(posindex,ele);
+		System.out.println(list);
+		System.out.println("");
+	}
+	public void search_Element(){
+		System.out.println("ENter the array elemenet to be searched : ");
+		String ele=in.next();
+		int index=list.indexOf(ele);
+		if(index==-1)
+			System.out.println("Element not found.");
+		else
+			System.out.println("Element found at index : "+index);
+	}
+	public void print(){
+		System.out.println("Enter the strting character to print strings ");
+		
+		char input_char=in.next().charAt(0);
+		String strc=Character.toString(input_char);
+		System.out.println("String starting with character "+strc);
+		for(int i=0;i<list.size();i++){
+			if(list.get(i).startsWith(strc)){
+				System.out.println(list.get(i));
+			}
+		}
+	}
+	public static void main(String args[]){
+		ArrayL obj=new ArrayL();
+		obj.array_display();
+		obj.append_at_end();
+		obj.insert_at_pos();
+		obj.search_Element();
+		obj.print();
+	}
+}	
+
